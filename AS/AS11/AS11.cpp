7779904f98de76ce54ebc6f6277c5fbe02c7c7ac
Rafael <amauri.rafael33@gmail.com>
2021-11-05 10:20:06 -0300

    feat: AS11 e gitignore

--- AS/AS11/AS11.cpp
+++ AS/AS11/AS11.cpp
@@ -10,11 +10,123 @@
 
 #define MAX_SIZE 1024
 
-int n,m,k,orig,dj[]={-2,-1,1,2,2,1,-1,-2},di[]={-1,-2,-2,-1,1,2,2,-1},memo[16][1<<16],adj[1<<4][1<<4],t[MAX_SIZE][MAX_SIZE],id;
+int n, m, k, orig, dj[] = {-2,-1,1,2,2,1,-1,-2};
+int di[] = {-1,-2,-2,-1,1,2,2,-1}, memo[16][1<<16], adj[1<<4][1<<4], t[MAX_SIZE][MAX_SIZE], id;
 
-char tab[MAX_SIZE][MAX_SIZE];
+char tabuleiro[MAX_SIZE][MAX_SIZE];
 
 std::vector<int> peca, dist;
+
+// Realiza uma busca por BFS
+void bfs(int s);
+
+int tsp(int u, int vis);
+
+int main()
+{
+    std::cin >> n >> m >> k;
+
+    while( (n + m + k) != 0 ){
+        
+        id = 1;
+        k++;
+        
+        for(int i = 0; i < k; i++)
+            for(int j = 0; j < k; j++)
+                adj[i][j] = (i != j) * INT_MAX;
+
+
+        for(int i = 1; i <= n; i++)
+        {
+            for(int j = 1; j <= m; j++)
+            {
+                std::cin >> tabuleiro[i][j];
+
+                switch (tabuleiro[i][j])
+                {
+                    case 'P':
+                        t[i][j] = id++;
+                        peca.push_back(i * (m+2) + j);
+                        break;
+                    
+                    case 'C':
+                        t[i][j] = 0;
+                        peca.insert(peca.begin(), (i)*(m+2)+(j));
+                        break;
+
+                    case '.':
+                        t[i][j] = -1;
+                        break;
+
+                    default:
+                        t[i][j] = -2;
+                        break;
+                }
+            }
+        }
+
+
+        for(int i = 0; i < k; i++)
+            bfs(peca[i]);
+
+
+        for(int i = k; i--;) 
+            for(int j = (1<<k); j > 0; j--) 
+                memo[i][j] = -1;
+
+
+        std::cout<<tsp(0,1)<<std::endl;
+        peca.clear();
+
+        std::cin >> n >> m >> k;
+    }
+
+    return 0;
+}
+
+void bfs(int s)
+{
+    std::queue<int> fila;
+    fila.push(s);
+
+    std::vector<int> dist = std::vector<int> ( (n + 2) * (m + 2) + 10, INT_MAX); 
+    dist[s] = 0;
+    s = t[s / (m+2)][s % (m+2)];
+    
+    while (!fila.empty())
+    {
+        int u = fila.front(); 
+        fila.pop();
+        
+        int i = u / (m+2);
+        int j = u % (m+2);
+
+        if (t[i][j] > -1)
+        {
+            int x = std::min(std::min(adj[s][t[i][j]], adj[t[i][j]][s]), dist[u]);
+            adj[s][t[i][j]] = adj[t[i][j]][s] = x;
+        }
+
+        int k = 8;
+
+        while(k > 0)
+        {
+            int a = i + di[k];
+            int b = j + dj[k];
+
+            int v = (a * (m+2) + b);
+            
+            if ((1<=(a) && (a)<=n && 1<=(b) && (b)<=m) && tabuleiro[a][b]!='#' && dist[v]==INT_MAX)
+            {
+                dist[v] = dist[u]+1;
+                fila.push(v);
+            }
+
+            k--;
+        }
+    }
+}
+
 
 int tsp(int u, int vis)
 {
@@ -26,88 +138,9 @@ int tsp(int u, int vis)
     
     int ans = INT_MAX;
     
-    for (int v = 0; v < k; v++)
+    for (int i = 0; i < k; i++)
     {
-        if (!(vis & (1 << v)))ans = std::min(ans, adj[u][v] + tsp(v,vis | (1<<v)));
+        if (!(vis & (1 << i)))ans = std::min(ans, adj[u][i] + tsp(i, vis | (1 << i)));
     }
     return memo[u][vis]=ans;
-}
-
-
-void bfs(int s)
-{
-    std::queue<int> fila;
-    fila.push(s);
-
-    std::vector<int> dist = std::vector<int> ((n+2) * (m+2) + 10, INT_MAX); 
-    dist[s]=0;
-    s=t[s / (m+2)][s % (m+2)];
-    
-    while (!fila.empty()){
-        int u=fila.front(); 
-        fila.pop();
-        
-        int i = u / (m+2);
-        int j = u % (m+2);
-
-        if (t[i][j]>-1)
-        {
-            int x = std::min(std::min(adj[s][t[i][j]], adj[t[i][j]][s]), dist[u]);
-            adj[s][t[i][j]] = adj[t[i][j]][s] = x;
-        }
-
-        for (int l=8; l--;){
-            int a = i+di[l];
-            int b = j+dj[l];
-
-            int v = (a * (m+2) + b);
-            
-            if ((1<=(a) && (a)<=n && 1<=(b) && (b)<=m) && tab[a][b]!='#' && dist[v]==INT_MAX)
-            {
-                dist[v]=dist[u]+1;
-                fila.push(v);
-            }
-        }
-    }
-}
-
-int main()
-{
-    while ((std::cin>>n>>m>>k) && (n+m+k)){
-        id = 1; ++k;
-        for (int i = 0; i < k; i++) for (int j = 0; j < k; j++) adj[i][j] = (i != j) * INT_MAX;
-
-        for (int i = 1;i <= n; i++){
-            for (int j = 1; j <= m; j++){
-                std::cin >> tab[i][j];
-                if (tab[i][j] == 'P')
-                {
-                    t[i][j] = id++;
-                    peca.push_back(i * (m+2) + j);
-                }
-                else if (tab[i][j] == 'C')
-                {
-                    t[i][j] = 0;
-                    peca.insert(peca.begin(), (i)*(m+2)+(j));
-                }
-                else if (tab[i][j] == '.')
-                {
-                    t[i][j] = -1;
-                }
-                else t[i][j] = -2;
-            }
-        }
-
-
-        for (int i=0; i<k; i++){
-            bfs(peca[i]);
-        }
-
-        for (int i=k; i--;) for (int j=(1<<k);j--;) memo[i][j]=-1;
-
-        std::cout<<tsp(0,1)<<std::endl;
-        peca.clear();
-    }
-
-    return 0;
 }
